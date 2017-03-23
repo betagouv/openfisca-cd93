602b68864706c17591abccd0d5c608e5939e9f03
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-03-23 11:09:27 +0100

    Make openfisca_cd93 a proper extension

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="Openfisca-CD93",
+    version="1.0.0",
+    description="Extension OpenFisca pour les aides sociales du conseil départemental de Seine Saint Denis",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    author="Incubateur de Services Numériques (SGMAP)",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
