acdddf38c3146a8ba5d917759c504630d10023fb
Thomas Guillet <guillet.thomas@gmail.com>
2017-07-19 14:14:39 +0200

    Upgrade Openfisca-Core to 15

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-CD93",
-    version="1.0.1",
+    version="1.0.2",
     description="Extension OpenFisca pour les aides sociales du conseil départemental de Seine Saint Denis",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Incubateur de Services Numériques (SGMAP)",
     packages=find_packages(),
     install_requires=[
-        'OpenFisca-Core >= 14, < 15',
+        'OpenFisca-Core >= 15, < 16',
         'OpenFisca-France >= 18, < 19'
     ],
     classifiers=[
